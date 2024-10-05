7e7dab52fe4ec5803a6bcab703f32324f8f676da
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-10-05 13:02:22 +0800

    added other required components and added the styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { Canvas } from "@react-three/fiber";
+import { Sky } from "@react-three/drei";
+import { Physics } from "@react-three/cannon";
+import { Ground } from "./Components/Ground";
+import { FPV as Fpv } from "./components/FPV.jsx";
+import { Player } from "./Components/Player.jsx";
+import { Cubes } from "./Components/Cubes.jsx";
+import { TextureSelector } from "./components/TextureSelect.jsx";
+import { Menu } from "./components/Menu.jsx";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <Fpv />
+        <Physics>
+          <Cubes />
+          <Player />
+          <Ground />
+        </Physics>
+      </Canvas>
+      <TextureSelector />
+      <Menu />
+      <div class="pointer">+</div>
+    </>
+  );
 }
 
 export default App;
