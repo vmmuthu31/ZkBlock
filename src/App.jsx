6830559752b810c45956bcd9a75ecfd305b152ee
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-10-05 13:06:30 +0800

    resolved the prod bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { Canvas } from "@react-three/fiber";
 import { Sky } from "@react-three/drei";
 import { Physics } from "@react-three/cannon";
 import { Ground } from "./Components/Ground";
-import { FPV as Fpv } from "./components/FPV.jsx";
+import { FPV as Fpv } from "./Components/FPV.jsx";
 import { Player } from "./Components/Player.jsx";
 import { Cubes } from "./Components/Cubes.jsx";
-import { TextureSelector } from "./components/TextureSelect.jsx";
-import { Menu } from "./components/Menu.jsx";
+import { TextureSelector } from "./Components/TextureSelect.jsx";
+import { Menu } from "./Components/Menu.jsx";
 
 function App() {
   return (
