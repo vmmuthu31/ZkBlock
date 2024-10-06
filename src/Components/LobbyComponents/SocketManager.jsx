58099bf13d027bda99a3b77efb338a8b5a09e1e1
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-10-06 10:24:41 +0800

    updated the route

--- src/Components/LobbyComponents/SocketManager.jsx
+++ src/Components/LobbyComponents/SocketManager.jsx
@@ -4,9 +4,7 @@ import { useEffect } from "react";
 import { io } from "socket.io-client";
 
 export const socket = io(
-  import.meta.env.VITE_SERVER_URL ||
-    "http://localhost:3000" ||
-    "https://api.zkblock.xyz/"
+  import.meta.env.VITE_SERVER_URL || "https://api.zkblock.xyz"
 );
 export const charactersAtom = atom([]);
 export const mapAtom = atom(null);
